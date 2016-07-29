d903c99895fd63b3b139c900a36f54d2f8564052
Nick Matvyeyev <mnasoft@gmail.com>
2016-07-29 16:19:11 +0300

    Проект в работоспособном состоянии

--- package.lisp
+++ package.lisp
@@ -1,8 +1,8 @@
 ;;;; package.lisp
 
 (defpackage #:ecology
-  (:use #:cl #:mnas-string #:postmodern #:hunchentoot #:cl-who)
+  (:use #:cl #:mnas-string #:mnas-site #:hunchentoot #:cl-who)
   (:export CO-ppm->mg/m3
 	   NOx-ppm->mg/m3)) 
 
-(declaim (optimize (speed 0) (safety 3) (debug 3)))
+;;;;(declaim (optimize (space 0) (compilation-speed 0)  (speed 0) (safety 3) (debug 3)))
